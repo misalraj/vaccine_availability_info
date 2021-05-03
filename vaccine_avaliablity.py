4bf2d6b83145fdd500be2566599f422c7a77ae00
Misal Raj <misalraj@gmail.com>
2021-05-03 15:47:49 +0530

    app updated

--- vaccine_avaliablity.py
+++ vaccine_avaliablity.py
@@ -42,42 +42,45 @@ with right_column_1:
 
 district_id = df_district_all.loc[df_district_all['district_name'] == selected_district, "district_id"].item()
 
-URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}" \
-    .format(district_id, today_date)
-res = requests.get(URL)
+try:
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}" \
+        .format(district_id, today_date)
+    res = requests.get(URL)
 
-calender_df = pd.DataFrame(json.loads(res.text)["centers"])
-district_pincode_list = calender_df.loc[calender_df['district_name'] == selected_district, "pincode"]
+    calender_df = pd.DataFrame(json.loads(res.text)["centers"])
+    district_pincode_list = calender_df.loc[calender_df['district_name'] == selected_district, "pincode"]
 
-calender_df["from"] = pd.to_datetime(calender_df["from"]).dt.strftime('%H:%M')
-calender_df["to"] = pd.to_datetime(calender_df["to"]).dt.strftime('%H:%M')
-calender_df["Timing"] = calender_df["from"] + " - " + calender_df["to"]
-calender_df.rename(columns={'name': 'Name', 'pincode': 'Pincode',
-                            'fee_type': 'Fee type', 'vaccine_fees': "Vaccine charge"}, inplace=True)
+    calender_df["from"] = pd.to_datetime(calender_df["from"]).dt.strftime('%H:%M')
+    calender_df["to"] = pd.to_datetime(calender_df["to"]).dt.strftime('%H:%M')
+    calender_df["Timing"] = calender_df["from"] + " - " + calender_df["to"]
+    calender_df.rename(columns={'name': 'Name', 'pincode': 'Pincode',
+                                'fee_type': 'Fee type', 'vaccine_fees': "Vaccine charge"}, inplace=True)
 
-new_df =calender_df.explode("sessions")
-new_df['Min Age Limit'] = new_df.sessions.apply(lambda x: x['min_age_limit'])
-new_df['Available Capacity'] = new_df.sessions.apply(lambda x: x['available_capacity'])
-new_df['Date'] = new_df.sessions.apply(lambda x: x['date'])
+    new_df =calender_df.explode("sessions")
+    new_df['Min Age Limit'] = new_df.sessions.apply(lambda x: x['min_age_limit'])
+    new_df['Available Capacity'] = new_df.sessions.apply(lambda x: x['available_capacity'])
+    new_df['Date'] = new_df.sessions.apply(lambda x: x['date'])
 
-if 'vaccine_fees' in new_df.columns:
-    new_df = new_df[['Date','Available Capacity', 'Min Age Limit',  'Name', 'Pincode', 'Timing', 'Fee type', 'Vaccine charge']]
-else:
-    new_df = new_df[['Date','Available Capacity', 'Min Age Limit','Name', 'Pincode', 'Timing', 'Fee type']]
+    if 'vaccine_fees' in new_df.columns:
+        new_df = new_df[['Date','Available Capacity', 'Min Age Limit',  'Name', 'Pincode', 'Timing', 'Fee type', 'Vaccine charge']]
+    else:
+        new_df = new_df[['Date','Available Capacity', 'Min Age Limit','Name', 'Pincode', 'Timing', 'Fee type']]
 
-selected_pincode = None
+    selected_pincode = None
 
-agree = st.checkbox('Filter by Pincode')
+    agree = st.checkbox('Filter by Pincode')
 
-if agree:
-    selected_pincode = st.selectbox(
-    "Select Pincode",
-    options=sorted(set(district_pincode_list.tolist())),
-    )
-    calender_df_pin = new_df[new_df["Pincode"] == selected_pincode]
-    st.success("Results: " + "Pincode" + ": " + str(selected_pincode) + ",   " + str(selected_district) + ", " + str(
-        selected_state))
-    st.table(calender_df_pin)
-else:
-    st.success("Results: " + str(selected_district) + ", " + str(selected_state), )
-    st.table(new_df)
+    if agree:
+        selected_pincode = st.selectbox(
+        "Select Pincode",
+        options=sorted(set(district_pincode_list.tolist())),
+        )
+        calender_df_pin = new_df[new_df["Pincode"] == selected_pincode]
+        st.success("Results: " + "Pincode" + ": " + str(selected_pincode) + ",   " + str(selected_district) + ", " + str(
+            selected_state))
+        st.table(calender_df_pin)
+    else:
+        st.success("Results: " + str(selected_district) + ", " + str(selected_state), )
+        st.table(new_df)
+except:
+    st.error('Unable to fetch data. Try after a few minutes')
